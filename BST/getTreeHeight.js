cba56dde2a98e31a9485eb32d96df4442966182a
renetchi <renetchi@gmail.com>
2021-01-16 18:59:00 +0900

    Add Hackerrank BST basics

---
+++ BST/getTreeHeight.js
@@ -0,0 +1,57 @@
+/*
+Day 22: Binary Search Trees
+https://www.hackerrank.com/challenges/30-binary-search-trees/problem
+
+The height of a binary search tree is the number of edges between the tree's root and its furthest leaf. 
+You are given a pointer, root, pointing to the root of a binary search tree. 
+Complete the getHeight function provided in your editor so that it returns the height of the binary search tree.
+*/
+
+// Start of function Node
+function Node(data) {
+  this.data = data;
+  this.left = null;
+  this.right = null;
+}; // End of function Node
+
+// Start of function BinarySearchTree
+function BinarySearchTree() {
+  this.insert = function(root, data) {
+      if (root === null) {
+          this.root = new Node(data);
+          
+          return this.root;
+      }
+      
+      if (data <= root.data) {
+          if (root.left) {
+              this.insert(root.left, data);
+          } else {
+              root.left = new Node(data);
+          }
+      } else {
+          if (root.right) {
+              this.insert(root.right, data);
+          } else {
+              root.right = new Node(data);
+          }
+      }
+      
+      return this.root;
+  };
+  
+  // Start of function getHeight
+  this.getHeight = function(root) {
+
+      // Add your code here
+      if(root === null) {
+          return -1;
+      }
+      
+      const leftDepth = this.getHeight(root.left);
+      const rightDepth = this.getHeight(root.right);
+      
+      return Math.max(leftDepth, rightDepth) + 1;
+
+  }; // End of function getHeight
+}; // End of function BinarySearchTree
---
+++ BST/levelOrderTraversal.js
@@ -0,0 +1,55 @@
+/**
+ * Day 23: BST Level-Order Traversal
+ * https://www.hackerrank.com/challenges/30-binary-trees/problem
+
+ Task
+A level-order traversal, also known as a breadth-first search, visits each level of a tree's nodes from left to right, top to bottom. 
+You are given a pointer, root, pointing to the root of a binary search tree. 
+Complete the levelOrder function provided in your editor so that it prints the level-order traversal of the binary search tree.
+
+Hint: You'll find a queue helpful in completing this challenge.
+
+ */
+
+function BinarySearchTree() {
+  this.insert = function(root, data) {
+      if (root === null) {
+          this.root = new Node(data);
+          
+          return this.root;
+      }
+      
+      if (data <= root.data) {
+          if (root.left) {
+              this.insert(root.left, data);
+          } else {
+              root.left = new Node(data);
+          }
+      } else {
+          if (root.right) {
+              this.insert(root.right, data);
+          } else {
+              root.right = new Node(data);
+          }
+      }
+      
+      return this.root;
+  };
+  
+  // Start of function levelOrder
+  this.levelOrder = function(root) {
+      const queue = [root];
+      while(queue.length > 0) {
+          const val = queue.shift();
+          process.stdout.write(val.data + ' ');
+          
+          if(val.left) {
+              queue.push(val.left);
+          }
+          if(val.right) {
+              queue.push(val.right);
+          }
+      }
+
+  }; // End of function levelOrder
+}; // End of function BinarySearchTree
